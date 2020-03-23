230bef63578797f42343339609c8af848bb4007e
antony <tonyjaenpop@gmail.com>
2020-03-23 03:37:55 -0600

    v1

---
+++ axi.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+from flask_cors import CORS
+import json
+#from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+@app.route('/',methods=['POST'])
+def Index():
+     # request.args.get('ID')
+    x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+    }  
+    y = json.dumps(x)
+    return y  
+
+if __name__ == '__main__':
+    app.run(port =3000, debug= True)
