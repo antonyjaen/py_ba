5514e61eb536d09526065de83db0ccec5bef15f0
antony <tonyjaenpop@gmail.com>
2020-03-23 14:06:20 -0600

    c3

--- axi.py
+++ axi.py
@@ -1,20 +1,23 @@
 from flask import Flask, request
 from flask_cors import CORS
+from db import *
 import json
+import pymysql
+
 #from flask_cors import CORS
+
+
 
 app = Flask(__name__)
 CORS(app)
-@app.route('/',methods=['POST'])
+@app.route('/',methods=['GET'])
 def Index():
      # request.args.get('ID')
-    x = {
-  "name": "John",
-  "age": 30,
-  "city": "New York"
-    }  
-    y = json.dumps(x)
-    return y  
+    db=database()
+    id=request.args.get('ID')
+    res = db.verificar(id)
+    print(res)
+    return res
 
 if __name__ == '__main__':
     app.run(port =3000, debug= True)
