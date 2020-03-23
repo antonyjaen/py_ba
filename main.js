b5fe720445e118b596fb1178d21ee2deef3688e0
antony <tonyjaenpop@gmail.com>
2020-03-23 14:37:35 -0600

    c3

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 $(document).ready(function () {
-   // const axios = require('axios');
-   
-    $("#ok").click(function () { 
-        alert("ok");
-        axios.post('http://127.0.0.1:3000/', {
-            params: {
-              ID: 12345
-            }
-          })
-          .then(function (response) {
-            console.log(response.data);
-            var ok= response.data;
-            alert(ok.name);
-          })
-          .catch(function (error) {
-            console.log("se produjo"+error);
-          })
-          .then(function () {
-            alert("termino");
-          });  
-    });
+  // const axios = require('axios');
+  
+   $("#ok").click(function () { 
+       alert("ok");
+       axios.get('http://127.0.0.1:3000/', {
+           params: {
+             ID: 2
+           }
+         })
+         .then(function (response) {
+           console.log(response.data);
+           var ok= response.data;
+           alert(ok.id);
+         })
+         .catch(function (error) {
+           console.log("se produjo"+error);
+         })
+         .then(function () {
+           alert("termino");
+         });  
+   });
 });
